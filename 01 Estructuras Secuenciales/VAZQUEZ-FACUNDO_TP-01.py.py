18531bd9a721e3f68b75dea077d6b3074f768c11
Facu Vazquez <104173020+facuvazquez1@users.noreply.github.com>
2025-03-24 11:23:03 -0300

    Add files via upload

---
+++ 01 Estructuras Secuenciales/VAZQUEZ-FACUNDO_TP-01.py.py
@@ -0,0 +1,129 @@
+""" 1) Crear un programa que imprima por pantalla el mensaje: “Hola Mundo! """
+
+# print("Hola Mundo!")
+
+#########################################################################
+
+""" 2) Crear un programa que pida al usuario su nombre e imprima por pantalla un saludo usando
+el nombre ingresado. Por ejemplo: si el usuario ingresa “Marcos”, el programa debe imprimir
+por pantalla “Hola Marcos!”. """
+
+# nombre = input("Ingrese su nombre: ")
+# print(f"Hola {nombre}!")
+
+#########################################################################
+
+""" 3) Crear un programa que pida al usuario su nombre, apellido, edad y lugar de residencia e
+imprima por pantalla una oración con los datos ingresados. Por ejemplo: si el usuario ingresa
+“Marcos”, “Pérez”, “30” y “Argentina”, el programa debe imprimir “Soy Marcos Pérez, tengo 30
+años y vivo en Argentina” """
+
+# nombre = input("Ingrese su nombre: ")
+# apellido = input("Ingrese su apellido: ")
+# edad = int(input("Ingrese su edad: "))
+# pais = input("Ingrese su lugar de residencia: ")
+
+# print(f"Soy {nombre} {apellido}, tengo {edad} años y vivo en {pais}.")
+
+#########################################################################
+
+"""" 4) Crear un programa que pida al usuario el radio de un círculo e imprima por pantalla su área y
+su perímetro """
+
+# radio = int(input("Ingrese el radio del circulo: "))
+# pi =  3.14159
+
+# area = pi * radio ** 2 
+# perimetro = 2 * pi * radio
+
+# print(f"El area del circulo es: {area} y su perimetro es {perimetro}.")
+
+#########################################################################
+
+
+"""" 5) Crear un programa que pida al usuario una cantidad de segundos e imprima por pantalla a
+cuántas horas equivale """
+
+# second = int(input("Ingresar cantidad de segundos: "))
+# hour = second / 3600
+
+# print(f"{second} segundos equivalen a {hour} horas.")
+
+
+#########################################################################
+
+"""" 6) Crear un programa que pida al usuario un número e imprima por pantalla la tabla de
+multiplicar de dicho número. """
+
+""" numero = int(input("Ingrese un numero: "))
+
+cero = numero * 0
+uno = numero * 1
+dos = numero * 2
+tres = numero * 3
+cuatro = numero * 4
+cinco = numero * 5
+seis = numero * 6
+siete = numero * 7
+ocho = numero * 8
+nueve = numero * 9
+diez = numero * 10
+
+print(f"La tabla de multiplicar de {numero} es: {cero}, {uno}, {dos}, {tres}, {cuatro}, {cinco}, {seis}, {siete}, {ocho}, {nueve} y {diez}") """
+
+
+#########################################################################
+
+""" 7) Crear un programa que pida al usuario dos números enteros distintos del 0 y muestre por
+pantalla el resultado de sumarlos, dividirlos, multiplicarlos y restarlos """
+
+""" numero1 = int(input("Ingrese el primer numero: "))
+numero2 = int(input("Ingrese el segundo numero: "))
+
+suma = numero1 + numero2
+division = numero1 / numero2
+multiplicacion = numero1 * numero2 
+resta = numero1 - numero2
+
+print(f"El resultado de distintas operaciones entre el {numero1} y {numero2}: suma = {suma}, division = {division}, multiplicacion = {multiplicacion} y resta = {resta}.") """
+
+#########################################################################
+
+""" 8) Crear un programa que pida al usuario su altura y su peso e imprima por pantalla su índice
+de masa corporal """
+
+""" altura = float(input("Ingrese su altura: "))
+peso = float(input("Ingrese su peso en kg: "))
+
+imc = peso / altura ** 2
+
+print(f"Su indice de masa corporal es de: {imc}.") """
+
+
+#########################################################################
+
+
+"""" 9) Crear un programa que pida al usuario una temperatura en grados Celsius e imprima por
+pantalla su equivalente en grados Fahrenheit """
+
+""" grados = float(input("Ingrese los grados C°: "))
+Fahrenheit = grados * 9 / 5 + 32
+
+print(f"La conversion de {grados} Celsius a Fahrenheit es: {Fahrenheit}") """
+
+
+#########################################################################
+
+""""" 10) Crear un programa que pida al usuario 3 números e imprima por pantalla el promedio de
+dichos números """
+
+""" numero1 = float(input("Ingrese el primer numero: "))
+numero2 = float(input("Ingrese el segundo numero: "))
+numero3 = float(input("Ingrese el tercer numero: "))
+
+promedio = (numero1 + numero2 + numero3) / 3
+
+print(f"El promedio de los tres numeros ingresados es: {promedio}") """
+
+
+
